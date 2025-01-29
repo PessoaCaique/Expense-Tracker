000f8c2cd7824d0f340a2b77c94e626982809493
Caique <caique.pessoa@kasznarleonardos.com>
2025-01-29 12:40:26 -0300

    Adição do .gitignore

--- Expense-Tracker/obj/Debug/net8.0/Expense-Tracker.AssemblyInfo.cs
+++ Expense-Tracker/obj/Debug/net8.0/Expense-Tracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Expense-Tracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35c5cfc33ef7ff63ea1ebdebcfa4e01f3a5cbecd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Expense-Tracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Expense-Tracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
